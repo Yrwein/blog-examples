a81d3dec21528b7c11f3f2aa397c08a284e37201
Josef Cech <czech.josef@gmail.com>
2020-01-18 14:24:19 +0100

    datajpatestflushandclear: Init commit

---
+++ datajpatestflushandclear/src/main/java/com/josefczech/blog/examples/datajpatestflushandclear/DatajpatestflushandclearApplication.java
@@ -0,0 +1,13 @@
+package com.josefczech.blog.examples.datajpatestflushandclear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatajpatestflushandclearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatajpatestflushandclearApplication.class, args);
+	}
+
+}
---
+++ datajpatestflushandclear/src/test/java/com/josefczech/blog/examples/datajpatestflushandclear/DatajpatestflushandclearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josefczech.blog.examples.datajpatestflushandclear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatajpatestflushandclearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
